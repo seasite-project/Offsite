f1fed83ec365c7830c0130f771fa02701fca3738
Johannes Seiferth <johannes.seiferth@uni-bayreuth.de>
2020-04-21 00:27:10 +0200

    Install script

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+cur_path=${PWD}
+
+# Default directories
+install_dir="$HOME/installkit"
+
+# Set PATH to include installed Python3 scripts.
+export PATH="${PATH}:~/.local/bin/"
+
+# Install wheel package required by setup.py.
+pip3 install --user wheel
+
+# Switch to installation directory.
+mkdir -p ${install_dir} 
+cd ${install_dir}
+
+# Install kerncraft.
+git clone https://github.com/RRZE-HPC/kerncraft
+cd kerncraft
+python3 setup.py bdist_wheel && pip3 install --user dist/kerncraft*.whl
+
+# Install IACA (required by kerncraft).
+iaca_get --I-accept-the-Intel-What-If-Pre-Release-License-Agreement-and-please-take-my-soul
+
+# Install Offsite.
+cd ${install_dir}
+git clone https://github.com/seasite-project/Offsite
+cd Offsite
+python3 setup.py bdist_wheel && pip3 install --user dist/offsite*.whl
+
+# Attention!
+echo ""
+echo "Note: Add ~/.local/bin to your PATH variable to run the installed Python3 scripts."
+
+cd ${cur_path}
